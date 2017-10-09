906e481a88358d6efa8258287dfd2dd85338f3f5
fanda <153304442@qq.com>
2017-10-09 19:08:19 +0800

    完善wx-app测试生物认证API提示

--- pages/news/news.js
+++ pages/news/news.js
@@ -497,6 +497,19 @@ Page({
               // console.log(res)
             }, fail(err){
               console.log(err)
+              wx.showModal({
+                title: '提示',
+                content: err.errMsg+'，重新认证？',
+                success: function (res) {
+                  if (res.confirm) {
+                    console.log('用户点击确定')
+                    self.checkIsSupportSoter()
+
+                  } else if (res.cancel) {
+                    console.log('用户点击取消')
+                  }
+                }
+              })
             }
           })
         }
